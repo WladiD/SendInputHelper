07bcdb6b7b4d653ce62e5865b3d178c163ff8005
Waldemar Derr <waldemar.derr@kind.com>
2017-12-21 09:24:26 +0100

    Initial code adjustments in the PingGoogle example after the import from sourceforge

--- Examples/PingGoogle/PingGoogle.dpr
+++ Examples/PingGoogle/PingGoogle.dpr
@@ -3,113 +3,103 @@
 {$APPTYPE CONSOLE}
 
 uses
-	SysUtils,
-	Windows,
-	SendInputHelper in '..\..\SendInputHelper.pas';
+  SysUtils,
+  Windows,
+  SendInputHelper in '..\..\SendInputHelper.pas';
 
 procedure Main;
 var
-	SIH:TSendInputHelper;
+  SIH: TSendInputHelper;
 
-	procedure SwitchWithTab;
-	begin
-		with SIH do
-		begin
-			AddShift([ssAlt], TRUE, FALSE);
-			AddDelay(50);
-			AddVirtualKey(VK_TAB, TRUE, FALSE);
-			AddDelay(50);
-			AddVirtualKey(VK_TAB, FALSE, TRUE);
-			AddShift([ssAlt], FALSE, TRUE);
-			AddDelay(50);
-		end;
-	end;
+  procedure SwitchWithTab;
+  begin
+    SIH.AddShift([ssAlt], True, False);
+    SIH.AddDelay(50);
+    SIH.AddVirtualKey(VK_TAB, True, False);
+    SIH.AddDelay(50);
+    SIH.AddVirtualKey(VK_TAB, False, True);
+    SIH.AddShift([ssAlt], False, True);
+    SIH.AddDelay(50);
+  end;
 
-	procedure SendLine(Line:String; Delay:Integer = 100);
-	begin
-		SIH.AddText(Line, TRUE);
-		SwitchWithTab;
-		SIH.AddText(Line, TRUE);
-		SwitchWithTab;
-		SIH.AddDelay(Delay);
-		SIH.Flush;
-	end;
+  procedure SendLine(Line: string; Delay: Integer = 100);
+  begin
+    SIH.AddText(Line, True);
+    SwitchWithTab;
+    SIH.AddText(Line, True);
+    SwitchWithTab;
+    SIH.AddDelay(Delay);
+    SIH.Flush;
+  end;
 
-	function IsWin7OrAbove:Boolean;
-	begin
-		Result:=((Win32MajorVersion * 1000) + Win32MinorVersion) >= 6001;
-	end;
+  function IsWin7OrAbove: Boolean;
+  begin
+    Result := ((Win32MajorVersion * 1000) + Win32MinorVersion) >= 6001;
+  end;
 begin
-	Writeln('A command shell and a notepad-instance will be launched [ENTER]');
-	Readln;
+  Writeln('A command shell and a notepad-instance will be launched [ENTER]');
+  Readln;
 
-	SIH:=TSendInputHelper.Create;
-	try
-		with SIH do
-		begin
-			// Start command shell
-			AddShortCut([ssWin], 'r');
-			AddDelay(100);
-			AddText('cmd', TRUE);
-			AddDelay(500);
-			// Align on the left screen side (shortcut available since Win 7)
-			if IsWin7OrAbove then
-			begin
-				AddShortCut([ssWin], VK_LEFT);
-				AddDelay(150);
-			end;
+  SIH := TSendInputHelper.Create;
+  try
+    // Start command shell
+    SIH.AddShortCut([ssWin], 'r');
+    SIH.AddDelay(100);
+    SIH.AddText('cmd', True);
+    SIH.AddDelay(500);
+    // Align on the left screen side (shortcut available since Win 7)
+    if IsWin7OrAbove then
+    begin
+      SIH.AddShortCut([ssWin], VK_LEFT);
+      SIH.AddDelay(150);
+    end;
 
-			// Start notepad
-			AddShortCut([ssWin], 'r');
-			AddDelay(100);
-			AddText('notepad', TRUE);
-			AddDelay(500);
-			// Align on the right screen side (shortcut available since Win 7)
-			if IsWin7OrAbove then
-			begin
-				AddShortCut([ssWin], VK_RIGHT);
-				AddDelay(150);
-			end;
+    // Start notepad
+    SIH.AddShortCut([ssWin], 'r');
+    SIH.AddDelay(100);
+    SIH.AddText('notepad', True);
+    SIH.AddDelay(500);
+    // Align on the right screen side (shortcut available since Win 7)
+    if IsWin7OrAbove then
+    begin
+      SIH.AddShortCut([ssWin], VK_RIGHT);
+      SIH.AddDelay(150);
+    end;
 
-			Flush;
-		end;
+    SIH.Flush;
 
-		SendLine('cls');
-		SendLine('ping google.de', 1000);
-		SendLine('ping gmail.com', 1000);
-		{**
-		 * Little promotion
-		 *}
-		with SIH do
-		begin
-			AddVirtualKey(VK_RETURN);
-			AddText('This is a example, how it can be easy', TRUE);
-			AddDelay(100);
-			AddText('to use the Windows.SendInput-API. ', TRUE);
-			AddVirtualKey(VK_RETURN);
-			AddText('Test of Unicode: ');
-			AddText('Привет! Cześć! 你好! !שלום', TRUE);
-			AddDelay(100);
-			AddVirtualKey(VK_RETURN);
-			AddText('Simplified by the SendInputHelper (Unit for Delphi):', TRUE);
-			AddDelay(100);
-			AddText('http://sourceforge.net/projects/sendinputhelper/');
+    SendLine('cls');
+    SendLine('ping google.de', 1000);
+    SendLine('ping gmail.com', 1000);
 
-			Flush;
-		end;
-	finally
-		SIH.Free;
-	end;
+    // Some self promo ;-)
+    SIH.AddVirtualKey(VK_RETURN);
+    SIH.AddText('This is a example, how it can be easy', True);
+    SIH.AddDelay(100);
+    SIH.AddText('to use the Windows.SendInput-API. ', True);
+    SIH.AddVirtualKey(VK_RETURN);
+    SIH.AddText('Test of Unicode: ');
+    SIH.AddText('Привет! Cześć! 你好! !שלום', True);
+    SIH.AddDelay(100);
+    SIH.AddVirtualKey(VK_RETURN);
+    SIH.AddText('Simplified by the SendInputHelper (Unit for Delphi):', True);
+    SIH.AddDelay(100);
+    SIH.AddText('https://github.com/WladiD/SendInputHelper');
 
-	WriteLn('All keystrokes was flushed. Press [Enter] to exit.');
-	Readln;
+    SIH.Flush;
+  finally
+    SIH.Free;
+  end;
+
+  WriteLn('All keystrokes was flushed. Press [Enter] to exit.');
+  Readln;
 end;
 
 begin
-	try
-		Main;
-	except
-		on E:Exception do
-			Writeln(E.ClassName, ': ', E.Message);
-	end;
+  try
+    Main;
+  except
+    on E:Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
 end.

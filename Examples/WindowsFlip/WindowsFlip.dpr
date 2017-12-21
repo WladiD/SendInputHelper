e273809dc2d779e95ebb6b5d4399afa4d7f4ba04
Waldemar Derr <waldemar.derr@kind.com>
2017-12-21 09:28:36 +0100

    Initial code adjustments in the WindowsFlip example after the import from sourceforge

--- Examples/WindowsFlip/WindowsFlip.dpr
+++ Examples/WindowsFlip/WindowsFlip.dpr
@@ -3,36 +3,38 @@ program WindowsFlip;
 {$APPTYPE CONSOLE}
 
 uses
-	SysUtils,
-	Windows,
-	SendInputHelper in '..\..\SendInputHelper.pas';
+  SysUtils,
+  Windows,
+  SendInputHelper in '..\..\SendInputHelper.pas';
 
 procedure Main;
 var
-	cc:Integer;
+  cc: Integer;
+  SIH: TSendInputHelper;
 begin
-	with TSendInputHelper.Create do
-	begin
-		AddShift([ssWin], TRUE, FALSE);
-		for cc:=1 to 20 do
-		begin
-			AddVirtualKey(VK_TAB, TRUE, FALSE);
-			AddDelay(100);
-		end;
-		AddVirtualKey(VK_TAB, FALSE, TRUE);
-		AddVirtualKey(VK_ESCAPE);
-		AddShift([ssWin], FALSE, TRUE);
-		Flush;
-		Free;
-	end;
+  SIH := TSendInputHelper.Create;
+  try
+    SIH.AddShift([ssWin], True, False);
+    for cc := 1 to 20 do
+    begin
+      SIH.AddVirtualKey(VK_TAB, True, False);
+      SIH.AddDelay(100);
+    end;
+    SIH.AddVirtualKey(VK_TAB, False, True);
+    SIH.AddVirtualKey(VK_ESCAPE);
+    SIH.AddShift([ssWin], False, True);
+    SIH.Flush;
+  finally
+    SIH.Free;
+  end;
 end;
 
 begin
-	try
-		Main;
-	except
-		on E: Exception do
-			Writeln(E.ClassName, ': ', E.Message);
-	end;
+  try
+    Main;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
 
 end.
